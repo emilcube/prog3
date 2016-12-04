63f3aae237d285a913a43aaaf1d666364f202d29
Emil <Emil@EMIL-PC>
2016-12-04 23:44:07 +0500

    zz

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -32,9 +32,6 @@
             this.redButton3 = new WindowsFormsApplication1.RedButton();
             this.redButton2 = new WindowsFormsApplication1.RedButton();
             this.redButton1 = new WindowsFormsApplication1.RedButton();
-            this.userControl13 = new WindowsFormsApplication1.UserControl1();
-            this.userControl12 = new WindowsFormsApplication1.UserControl1();
-            this.userControl11 = new WindowsFormsApplication1.UserControl1();
             this.SuspendLayout();
             // 
             // grid1
@@ -42,7 +39,7 @@
             this.grid1.CellSize = 30;
             this.grid1.ColumnCount = 3;
             this.grid1.LineColor = System.Drawing.Color.Red;
-            this.grid1.Location = new System.Drawing.Point(394, 37);
+            this.grid1.Location = new System.Drawing.Point(245, 30);
             this.grid1.Name = "grid1";
             this.grid1.RowCount = 5;
             this.grid1.Size = new System.Drawing.Size(171, 158);
@@ -64,7 +61,7 @@
             // 
             this.redButton2.BackColor = System.Drawing.Color.Red;
             this.redButton2.Crossed = false;
-            this.redButton2.Location = new System.Drawing.Point(490, 167);
+            this.redButton2.Location = new System.Drawing.Point(557, 219);
             this.redButton2.Name = "redButton2";
             this.redButton2.Size = new System.Drawing.Size(75, 75);
             this.redButton2.TabIndex = 4;
@@ -84,32 +81,6 @@
             this.redButton1.UseVisualStyleBackColor = false;
             this.redButton1.Click += new System.EventHandler(this.redButton1_Click);
             // 
-            // userControl13
-            // 
-            this.userControl13.BCD = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.userControl13.Location = new System.Drawing.Point(41, 219);
-            this.userControl13.Name = "userControl13";
-            this.userControl13.Size = new System.Drawing.Size(210, 147);
-            this.userControl13.TabIndex = 2;
-            // 
-            // userControl12
-            // 
-            this.userControl12.BCD = System.Drawing.Color.Empty;
-            this.userControl12.Location = new System.Drawing.Point(220, 83);
-            this.userControl12.Name = "userControl12";
-            this.userControl12.Size = new System.Drawing.Size(233, 143);
-            this.userControl12.TabIndex = 1;
-            // 
-            // userControl11
-            // 
-            this.userControl11.BCD = System.Drawing.Color.Empty;
-            this.userControl11.Location = new System.Drawing.Point(12, 12);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(199, 147);
-            this.userControl11.TabIndex = 0;
-            this.userControl11.ButtonClicked += new System.EventHandler(this.userControl11_ButtonClicked);
-            this.userControl11.Load += new System.EventHandler(this.MYEVENTHANDLER);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -119,9 +90,6 @@
             this.Controls.Add(this.redButton3);
             this.Controls.Add(this.redButton2);
             this.Controls.Add(this.redButton1);
-            this.Controls.Add(this.userControl13);
-            this.Controls.Add(this.userControl12);
-            this.Controls.Add(this.userControl11);
             this.Name = "Form1";
             this.RightToLeftLayout = true;
             this.Text = "Form1";
@@ -131,10 +99,6 @@
         }
 
         #endregion
-
-        private UserControl1 userControl11;
-        private UserControl1 userControl12;
-        private UserControl1 userControl13;
         private RedButton redButton1;
         private RedButton redButton2;
         private RedButton redButton3;
